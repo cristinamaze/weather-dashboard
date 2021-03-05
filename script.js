f71711c250dfbebf370cc8def8a40c1f6b2cbb08
cristinamaze <76184003+cristinamaze@users.noreply.github.com>
2021-03-05 16:23:49 -0500

    ajax instead of fetch

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-var fetchButton = document.getElementById("fetch-button");
-var city = "text"
+$(document).ready(function() {
 
-fetch("https://pro.openweathermap.org/data/2.5/forecast/hourly?q=" + city + "&appid=26bbe025e9f5a2567433d6f2ec405c5e", {
-    cache: "reload",
+console.log("hello")
+
+var searchButton = $("#search-button")
+searchButton.on("click", searchFunction)
+
+function searchFunction() {
+    var searchCity = "miami"
+    console.log(searchCity)
+
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=26bbe025e9f5a2567433d6f2ec405c5e&units=imperial",
+            dataType: "json",
+            success: function(data) {
+                console.log(data)
+}
+})}
 })
-  .then(function (response) {
-      return response.json();
-  })
-  .then(function (data) {
-      console.log(data);
-  });
+//var city = "#city-search";
+// $("searchbar")
+
+// fetch("https://api.openweathermap.org/data/2.5/forecast/hourly?q=" + city + "&APPID=e025e9f5a2567433d6f2ec405c5e", {
+
+ // parseInit  // cache: "reload"
