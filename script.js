fcb191d1c90424395f9f3b2e09138cb98b5e0b7a
cristinamaze <76184003+cristinamaze@users.noreply.github.com>
2021-03-12 16:25:51 -0500

    data functions for variables

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 
     console.log("hello")
-
+    var history=JSON.parse(localStorage.getItem("history"))||[] 
     var searchButton = $("#search-button")
     searchButton.click(searchFunction)
 
     function searchFunction(event) {
         event.preventDefault()
-        var searchCity = "miami"
+        var searchCity = $("#search-city").val()
         console.log(searchCity)
+        history.push(searchCity)
+        localStorage.setItem("history", JSON.stringify(history))
 
         $.ajax({
             type: "GET",
@@ -16,7 +18,27 @@ $(document).ready(function() {
             dataType: "json",
             success: function(data) {
                 console.log(data)
+                searchUV(data.coord.lon, data.coord.lat)
+                displayWeather(data)
+            }
 
+        })
+    }
+
+    function displayWeather(data) {
+        $("#cityName").text(data.name)
+        $("#temp").text(data.main.temp + " degrees")
+        $("#humidity").text("humidity: " + data.main.humidity + "%")
+        $("#wind-speed").text("wind speed: " + data.wind.speed + "mph")
+    }
+
+    function searchUV(lat, lon) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/uvi?appid=26bbe025e9f5a2567433d6f2ec405c5e&lat=" + lat + "&lon=" + lon,
+            dataType: "json",
+            success: function(data) {
+                console.log(data)
             }
 
         })
